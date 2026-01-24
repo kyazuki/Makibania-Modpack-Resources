a3bed5237e4cae8e78d77e394f79482f7dae522b
kyazuki <20392096+kyazuki@users.noreply.github.com>
2026-01-24 10:40:43 +0900

    Powershell版のインストールスクリプトを追加

---
+++ client/Installer.ps1
@@ -0,0 +1,11 @@
+﻿# インストーラーをダウンロードして実行
+Write-Host "構成情報をダウンロード中..."
+try {
+    Invoke-WebRequest -Uri "https://github.com/kyazuki/Makibania-Modpack-Resources/releases/latest/download/run.ps1" -OutFile "$PSScriptRoot\run.ps1" -ErrorAction Stop
+}
+catch {
+    Write-Error ("構成情報のダウンロードに失敗しました。詳細: {0}" -f $_.Exception.Message)
+    Read-Host "Press Enter key to exit..."
+    exit 1
+}
+& "$PSScriptRoot\run.ps1"
---
+++ client/run.ps1
@@ -0,0 +1,21 @@
+﻿# パック設定をダウンロード
+Write-Host "パック設定をダウンロード中..."
+try {
+    Invoke-WebRequest -Uri "https://github.com/kyazuki/Makibania-Modpack-Resources/releases/download/v1.3.2/config.yaml" -OutFile "$PSScriptRoot\config.yaml" -ErrorAction Stop
+}
+catch {
+    Write-Error ("パック設定のダウンロードに失敗しました。詳細: {0}" -f $_.Exception.Message)
+    Read-Host "Press Enter key to exit..."
+    exit 1
+}
+# インストーラーをダウンロードして実行
+Write-Host "インストーラーをダウンロード中..."
+try {
+    Invoke-WebRequest -Uri "https://github.com/medi-torimorta/Makibania-Modpack-Installer/releases/download/v3.2.1%2B1.3.2/makibania-modpack-installer.exe" -OutFile "$PSScriptRoot\makibania-modpack-installer.exe" -ErrorAction Stop
+}
+catch {
+    Write-Error ("インストーラーのダウンロードに失敗しました。詳細: {0}" -f $_.Exception.Message)
+    Read-Host "Press Enter key to exit..."
+    exit 1
+}
+Start-Process -FilePath "$PSScriptRoot\makibania-modpack-installer.exe" -NoNewWindow
